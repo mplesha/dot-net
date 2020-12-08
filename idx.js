5e219444c8c6cddae02e9de8d447950bfca3d42f
mplesha <mykhaylo.plesha@gmail.com>
2020-12-08 22:59:55 +0200

    Update idx.js

--- idx.js
+++ idx.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const spawn = require('child_process');
+const { spawn } = require('child_process');
 
 const data = fs.readFileSync(path.resolve('.github/classroom/autograding.json'));
 const json = JSON.parse(data.toString());
