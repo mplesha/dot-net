d3012a115a7289d95c506bbfe1d109b65d2950ea
mplesha <mykhaylo.plesha@gmail.com>
2020-12-08 23:22:04 +0200

    Update idx.js

--- idx.js
+++ idx.js
@@ -5,5 +5,4 @@ const { spawn } = require('child_process');
 const data = fs.readFileSync(path.resolve('.github/classroom/autograding.json'));
 const json = JSON.parse(data.toString());
 
-spawn('dotnet test -t >> prt.txt', {shell: true});
-spawn('echo ###### >> prt.txt', {shell: true});
+spawn('dotnet test -t >> prt.txt && echo ###### >> prt.tx', {shell: true});
